5b4affe4c18a582f05811662b5579f08531cd502
EC2 Default User <ec2-user@ip-172-31-27-177.ap-northeast-1.compute.internal>
2023-03-05 13:38:50 +0000

    user.rbのバリデーション修正しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,26 @@ class User < ApplicationRecord
    validates :starttime, presence: true
    validates :endtime, presence: true
    validates :memo,length: {maximum: 500 }
-   
-   validate :start_end_check
-   validate :startday
 
-    def start_end_check
-      if self.endtime < self.starttime
-       errors.add(:endtime, "は開始日より前の日付は登録できません。") 
-      end
-    end
+   validate :valid_eligiblity_range?
+   validate :pretend_ago
 
-    def startday
-      return if starttime.blank?
-      errors.add(:starttime, "は今日以降のものを選択してください") if starttime < Date.today
+def pretend_ago
+    today = Date.today
+    
+    return unless starttime? && today
+    
+    if !starttime.after?(today)
+    errors.add(:starttime, 'must be after Today')
     end
+end
+
+  def valid_eligiblity_range?
+    return unless endtime? && starttime?
+
+    if !endtime.after?(starttime)
+      errors.add(:starttime, "must be before Expiration Date")
+      errors.add(:endtime, "must be after Start Date")
+    end
+  end
 end
